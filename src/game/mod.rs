fc459c4821ea42a2cd7feeab3792f46e60facb10
Niclas Gustafsson <niclas.je.gustafsson@gmail.com>
2024-09-07 12:10:03 +0200

    Refactor game module methods + add new methods for game control. Refactor game loop into run function.

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -3,8 +3,8 @@ use std::process;
 use colored::*;
 use rand::*;
 
-#[derive(Debug)]
-enum GameStatus {
+#[derive(PartialEq, Debug)]
+pub enum GameStatus {
     IsGame,
     IsNotGame,
     IsNewGame
@@ -26,25 +26,63 @@ impl Game {
     pub fn set_answer(&mut self) {
         let answer: u8 = rand::thread_rng().gen_range(1..=20);
         self.answer = answer;
+        self.update_game_state(GameStatus::IsGame);
     }
 
-    pub fn make_guess(&mut self)-> &str{
+    fn update_game_state(&mut self, state: GameStatus) {
+        self.state = state;
+    }
+    //Reset current game object.
+    pub fn new_game(&mut self) {
+        self.answer = 0;
+        self.guess = 0;
+        self.total_guesses = 5;
+        self.guesses_made = 0;
+        self.state = GameStatus::IsNewGame
+    }
+
+    pub fn run(&mut self) {
+        while self.state == GameStatus::IsNewGame || self.state == GameStatus::IsGame {
+
+            if self.state == GameStatus::IsNewGame {
+                //if new game reset stats and set new answer?
+                self.new_game();
+                self.set_answer();
+            } else if self.state == GameStatus::IsGame {
+                println!("Guesses left: {}.\nType 'quit' to exit.\nEnter a number between 1-20:", self.total_guesses - self.guesses_made);
+                self.make_guess();
+  
+                self.check_guess();
+    
+                if self.guesses_made == self.total_guesses {
+                    self.state = GameStatus::IsNotGame;
+                }
+            } 
+        } 
+
+        if self.state == GameStatus::IsNotGame {
+            self.ask_for_new_game();
+    
+            self.run();
+        }
+    }
+
+
+    //Takes user input and tries parsing to number.
+    pub fn make_guess(&mut self){
         let mut guess = String::new();
         
         io::stdin().read_line(&mut guess).expect("Failed to read guess.");
         let guess = guess.trim();
 
-        guess
+        // guess
         // println!("Guess from make_guess {}", guess);
-        // let parse_ok = self.parse_guess(&guess);
+        let parse_ok = self.parse_guess(&guess);
 
-        // if parse_ok.is_err() {
-        //     println!("Please insert a number and try again");
-        //     self.make_guess();
-        // }
-
-
-       
+        if parse_ok.is_err() {
+            println!("Please insert a number and try again:");
+            self.make_guess();
+        }
     }
 
     fn parse_guess(&mut self, guess: &str) -> Result<(), &str>{
@@ -61,17 +99,31 @@ impl Game {
            Err("Couldn't parse number.")
         }
     }
+    pub fn ask_for_new_game(&mut self) {
+        let mut answer = String::new();
+        println!("Do you want to play again? Type y/yes, n/no");
+        io::stdin().read_line(&mut answer).expect("Failed to read guess.");
+        let answer = answer.trim().to_lowercase();
 
-    pub fn check_guess(&self) {
+        if answer == "y" || answer == "yes" {
+            //start new game
+            // Self::update_game_state(self, GameStatus::IsNotGame);
+            Self::update_game_state(self, GameStatus::IsNewGame);
+        } else if answer == "n" || answer == "no" {
+            process::exit(0);
+        }
+    }
+
+
+    pub fn check_guess(&mut self) {
         match self.guess.cmp(&self.answer) {
             Ordering::Less => println!("{}", "Too small".red()),
             Ordering::Greater => println!("{}", "Too big".red()),
             Ordering::Equal => {
                 println!("{}", "You win!".green());
-                  
+                Self::update_game_state(self, GameStatus::IsNotGame);   
             },
         }
     }
 
- 
 }
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,9 @@ mod game;
 use game::*;
 
 fn main() {
-let mut game = game::Game::new();
-Game::set_answer(&mut game);
-println!("Game object: {:?}", game);
-println!("Answer: {:?}", game.answer);
-    println!("-----------------GUESS THE NUMBER------------------");
-    println!("Make a guess between 1-20 in number. E.g 4, 14, 1");
-    Game::make_guess(&mut game);
-    // println!("Game object: {:?}", game);
-
-    //Parse guess and make comparison
+    //Initiate game object and set answer
+    let mut game = Game::new();
+    Game::set_answer(&mut game);
+    //Initiate game loop
+    game.run();
 }
